484ac7d35b5befffaeca8f682fcfa5e130718374
Xinkai Jiang <jiangxkjohn@gmail.com>
2023-11-03 14:26:04 +0100

    first commit

---
+++ Base.py
@@ -0,0 +1,199 @@
+from websockets import server
+import asyncio
+from asyncio import sleep as async_sleep
+import threading
+import json
+import abc
+
+
+class ServerBase(abc.ABC):
+    """
+    A abstract class for all server running on a simulation environment.
+
+    The server receives and sends messages to client by websockets.
+
+    Args:
+        host (str): the ip address of service
+        port (str): the port of service
+    """    
+    def __init__(
+        self, 
+        host: str, 
+        port: str
+    ) -> None:
+
+        self._ws: server.WebSocketServerProtocol = None
+        self._wsserver: server.WebSocketServer = None
+        self._server_future: asyncio.Future = None
+        self.host = host
+        self.port = port
+        
+    def start_server_thread(self, block = False) -> None:
+        """
+        Start a thread for service.
+
+        Args:
+            block (bool, optional): main thread stop running and 
+            wait for server thread. Defaults to False.
+        """
+        self.server_thread = threading.Thread(target=self._start_server_task)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+        if block:
+            self.server_thread.join()
+
+    @abc.abstractmethod
+    def process_message(self, msg:str) -> None:
+        """
+        Processing message when receives new messages from clients.
+
+        Args:
+            msg (str): message from client.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def create_handler(self, ws: server.WebSocketServerProtocol) -> list[asyncio.Task]:
+        """
+        Create new tasks such as receiving and stream when start service.
+
+        Args:
+            ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+
+        Returns:
+            list[asyncio.Task]: the list of new tasks
+        """        
+        raise NotImplementedError
+
+    async def receive_handler(self, ws: server.WebSocketServerProtocol):
+        """
+        Default messages receiving task handler.
+
+        Args:
+        ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+        """        
+        async for msg in ws:
+            self.process_message(msg)
+
+    async def handler(self, ws: server.WebSocketServerProtocol):
+        """
+        The main task handler of loop in server thread.
+
+        Args:
+            ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+        """        
+        self.on_conncet(ws)
+        _, pending = await asyncio.wait(
+            self.create_handler(ws),
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        for task in pending:
+            task.cancel()
+        await ws.close()
+        self.on_close(ws)
+
+    async def _expect_client(self):
+        """
+        The coroutine task for running service on loop.
+        """        
+        self.stop = asyncio.Future()
+        async with server.serve(self.handler, self.host, self.port) as self._wsserver:
+            await self.stop
+
+    def _start_server_task(self) -> None:
+        """
+        The thread task for running a service loop.
+        """        
+        print(f"start a server on {self.host}:{self.port}")
+        asyncio.run(self._expect_client())
+        print("server task finished")
+
+    async def _send_str_msg_on_loop(
+        self, msg: str, 
+        ws: server.WebSocketServerProtocol, 
+        sleep_time: float = 0
+    ):
+        """
+        Send a str message
+         
+        It can only be used in the server thread.
+
+        Args:
+            msg (str): message to be sent.
+            ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+            sleep_time (float, optional): sleep time after sending the message. Defaults to 0.
+        """        
+        await ws.send(str.encode(json.dumps(msg)))
+        await async_sleep(sleep_time)
+ 
+    def send_str_msg(self, msg: str, sleep_time: float = 0) -> None:
+        """
+        Send a str message outside the server thread.
+
+        Args:
+            msg (str): message to be sent.
+            sleep_time (float, optional): sleep time after sending the message. Defaults to 0.
+        """            
+        if self._ws is None:
+            return
+        self.create_new_task(self._send_str_msg_on_loop(msg, self._ws, sleep_time))
+
+    def create_new_task(self, task: asyncio.Task) -> None:
+        """
+        Create a new task on the service loop.
+
+        Args:
+            task (asyncio.Task): Task to be ruuning on the loop.
+        """        
+        loop = self._wsserver.get_loop()
+        loop.create_task(task)
+
+
+    def close_server(self) -> None:
+        """
+        Stop and close the server.
+        """        
+        self._wsserver.close()
+
+    def on_conncet(self, ws: server.WebSocketServerProtocol) -> None:
+        """
+        This method will be executed once the connection is established.
+
+        Args:
+            ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+        """        
+        print(f"connected by: {ws.local_address}")
+        self._ws = ws
+
+    def on_close(self, ws: server.WebSocketServerProtocol) -> None:
+        """
+        This method will be executed when disconnected.
+
+        Args:
+            ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+        """        
+        self._ws = None
+        print(f"the connection to {ws.local_address} is closed")
+
+class ObjectSerializerBase(abc.ABC):
+    """
+    A abstract class for serializing simulation objects to json, and 
+    then transmit their state by the server.
+    
+    Args:
+        id (str): the id of this object.
+    """    
+    def __init__(
+        self, 
+        id: str,
+    ) -> None:
+        self.id: str = id
+
+    @abc.abstractclassmethod
+    def get_obj_param_dict(self) -> dict:
+        raise NotImplemented
+
+    @abc.abstractclassmethod
+    def get_obj_state_dict(self) -> dict:
+        raise NotImplemented
+
---
+++ Primitive.py
@@ -0,0 +1,138 @@
+import asyncio
+from websockets import server
+from time import localtime, strftime
+
+from .Base import ServerBase, ObjectSerializerBase
+
+class PrimitiveServer(ServerBase):
+    """
+    A basic implementation of server for receiving and printing messages.
+    """    
+    def __init__(
+        self,
+        host="127.0.0.1",
+        port=8052,
+    ) -> None:
+        super().__init__(host, port)
+
+    def process_message(self, msg:str) -> None:  
+        print(f"new message: {msg}")
+
+    def create_handler(self, ws: server.WebSocketServerProtocol) -> list[asyncio.Task]:
+        """
+        Create a new receiving task for server.
+
+        Args:
+            ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+
+        Returns:
+            list[asyncio.Task]: the list of new tasks
+        """        
+        return [asyncio.create_task(self.receive_handler(ws))]
+
+
+class ObjectStreamer(PrimitiveServer):
+    """
+    A basic server for streaming data and receiving messages.
+
+    Args:
+        dt (float): message stream interval (second). Defaults to 0.05.
+        on_stream (bool): whether starting stream right now.
+    """    
+    def __init__(
+            self,
+            dt: float =  0.05,
+            host = "127.0.0.1", 
+            port = 8052,
+            on_stream: bool = False,
+        ) -> None:
+        super().__init__(host, port)
+        
+        # flags
+        self.on_stream = on_stream
+
+        # stream frequency
+        self.dt = dt
+
+    def update_stream_data(self) -> str:
+        """
+        Update data for stream.
+
+        Returns:
+            str: the data to be sent
+        """        
+        current_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        return f"this is stream message {current_time}"
+
+    def start_stream(self) -> None:
+        """
+        Start stream.
+        """        
+        if self._ws is None:
+            return
+        self.create_new_task(self.stream_handler(self._ws, self.dt))
+        self.on_stream = True
+
+    def close_stream(self) -> None:
+        """
+        Close stream.
+        """        
+        self.on_stream = False
+
+    def on_start_stream(self) -> None:
+        """
+        This method will be executed once the stream is started.
+        """        
+        print("start stream")
+
+    def on_close_stream(self) -> None:
+        """
+        This method will be executed once the stream is closed
+        """        
+        self.on_stream = False
+        print("close stream")
+
+    async def stream_handler(self, ws: server.WebSocketServerProtocol, dt:float):
+        """
+        Default message stream task handler.
+
+        Args:
+        ws (server.WebSocketServerProtocol): WebSocketServerProtocol from websockets.
+        """      
+        self.on_start_stream()
+        while self.on_stream:
+            stream_data = self.update_stream_data()
+            try:
+                await self._send_str_msg_on_loop(stream_data, ws, dt)
+            except:
+                print("error occured when sending messages!!!!!")
+                await ws.close()
+                break
+            finally:
+                pass
+        self.on_close_stream()
+
+class ObjectSerializer(ObjectSerializerBase):
+    """
+    A new class for serializing simulation objects to json, and 
+    then transmit their state by the server.
+
+    Args:
+        id (str): the id of this object.
+        parent (ObjectSerializerBase, optional): the parent of this object. Defaults to None.
+        child (ObjectSerializerBase, optional): the child of this object. Defaults to None.
+    """    
+    def __init__(
+        self, 
+        id: str,
+        parent: ObjectSerializerBase = None,
+        child: ObjectSerializerBase = None,
+    ) -> None:
+        super().__init__(id)
+        self.parent = parent
+        self.child = child
+
+
+if __name__ == "__main__":
+    s = ObjectStreamer()
+    s.start_server_thread(block=True)
