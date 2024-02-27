8b4819a4d4fab6766a665e70e8003c680a800da8
Xinkai Jiang <jiangxkjohn@gmail.com>
2024-02-27 18:01:14 +0100

    mujoco scene loader

--- sim_pub/model_loader/ucomponent.py
+++ sim_pub/model_loader/ucomponent.py
@@ -4,7 +4,7 @@ from enum import Enum
 import abc
 from typing import List, Optional, Tuple, TypeVar
 
-from utils import singleton
+from .utils import singleton
 
 
 class UHeaderType(str, Enum):
@@ -44,7 +44,8 @@ class UMetaEntity(abc.ABC):
 
 @dataclass
 class UComponent(UMetaEntity):
-	pass
+	def to_string(self) -> str:
+		return super().to_string()
 
 class UMaterial(UComponent):
 
@@ -74,22 +75,43 @@ class UVisual(UComponent):
 	scale : List[float]
 	meshes : List[UMesh]
 
+class UGameObjectBase(UMetaEntity):
 
-class UGameObject(UMetaEntity):
-
-	def __init__(
-			self
-			) -> None:
+	def __init__(self) -> None:
 		super().__init__()
 		self.pos: list[float] = [0, 0, 0]
 		self.rot: list[float] = [0, 0, 0]
-		self.visual : UVisual
-		self.parent: UGameObject = SceneRoot.instance()
-		self.children: List[UMetaEntity] = list()
+		self.static: bool = False
+		self.parent: UGameObjectBase
 
 	def to_string(self) -> str:
 		return super().to_string()
 
+class UGameObject(UGameObjectBase):
+	def __init__(
+		self,
+		parent: UGameObjectBase,
+	) -> None:
+		super().__init__()
+		self.parent = parent
+		self.visual_child: UGameObject = None
+		self.children: List[UGameObjectBase] = list()
+
+	def add_child(self, child: UGameObjectBase) -> None:
+		self.children.append(child)
+
+	def add_visual(self, visual: UVisual):
+		if self.visual_child is None:
+			self.visual_child = UGameObject(parent=self)
+		visual_object = UVisualGameObject(self.visual_child)
+		visual_object.visual = visual
+		self.visual_child.add_child(visual_object)
+
+
+class UVisualGameObject(UGameObject):
+	def __init__(self, parent: UGameObject) -> None:
+		super().__init__(parent)
+		self.visual: UVisual
 
 @singleton
 class EmptyGameObject(UGameObject):
@@ -97,7 +119,9 @@ class EmptyGameObject(UGameObject):
 
 @singleton
 class SceneRoot(UGameObject):
-	pass
+	def __init__(self) -> None:
+		super().__init__(parent=None)
+
 
 class UJoint(UMetaEntity):
 	def __init__(self) -> None:
