7be09e370e136ce6b782193d941563a218fb4f2a
Xinkai Jiang <jiangxkjohn@gmail.com>
2024-03-01 16:01:23 +0100

    serialize the game objects

--- sim_pub/model_loader/utils.py
+++ sim_pub/model_loader/utils.py
@@ -1,3 +1,5 @@
+from xml.etree.ElementTree import Element as XMLNode
+
 def singleton(cls):
     instances = {}
     def get_instance(*args, **kwargs):
@@ -5,3 +7,14 @@ def singleton(cls):
             instances[cls] = cls(*args, **kwargs)
         return instances[cls]
     return get_instance
+
+
+
+def get_name(node: XMLNode, default_name: str = "default") -> str:
+    return node.get("name", default_name)
+
+def get_pos(node: XMLNode) -> list[float]:
+    return list(map(float, node.get("pos", "0.0 0.0 0.0").split()))
+
+def get_rot(node: XMLNode) -> list[float]:
+    return list(map(float, node.get("rot", "0.0 0.0 0.0").split()))
