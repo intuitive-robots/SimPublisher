9dcb23da0d338a98dea4a0d89391993bd2783bcc
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-03-19 18:02:26 +0100

    mjcf parser

--- tests/model_test/test_model_loader.py
+++ tests/model_test/test_model_loader.py
@@ -8,12 +8,12 @@ from sim_pub.mujoco.mujoco_streamer import MujocoStreamer
 model_path = os.path.join(os.path.dirname(__file__), "../../models/static_scene/static_scene.xml")
 model_path = os.path.abspath(model_path)
 # # Create a viewer to visualize the simulation
-model = mujoco.MjModel.from_xml_path(model_path)
+# model = mujoco.MjModel.from_xml_path(model_path)
+# data = mujoco.MjData(model)
 
-data = mujoco.MjData(model)
-
+loader = MJCFLoader(model_path)
 # Make renderer, render and show the pixels
-renderer = mujoco.Renderer(model)
+# renderer = mujoco.Renderer(model)
 # mujoco.mj_forward(model, data)
 # renderer.update_scene(data)
 
@@ -26,12 +26,12 @@ renderer = mujoco.Renderer(model)
 # viewer = mujoco.MjViewer(sim)
 
 # Run the simulation for 1000 steps
-def step_fn(physics, random_state):
-    # Here you can apply forces, compute rewards, etc.
-    pass
+# def step_fn(physics, random_state):
+#     # Here you can apply forces, compute rewards, etc.
+#     pass
 
-# Create a viewer and pass the physics and step function
-viewer.launch(model, step_fn)
+# # Create a viewer and pass the physics and step function
+# viewer.launch(model, step_fn)
 
 # # Close the viewer
 # viewer.close()
