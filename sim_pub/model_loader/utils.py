60c077a2eed7b4b980381a795291b44ed7ea3f89
Xinkai Jiang <jiangxkjohn@gmail.com>
2024-03-28 13:51:36 +0100

    mesh

--- sim_pub/model_loader/utils.py
+++ sim_pub/model_loader/utils.py
@@ -1,4 +1,5 @@
 from xml.etree.ElementTree import Element as XMLNode
+from typing import List
 
 def singleton(cls):
     instances = {}
@@ -8,15 +9,25 @@ def singleton(cls):
         return instances[cls]
     return get_instance
 
+def ros2unity(array: List[float]):
+    return [-array[1], array[2], array[0]]
 
-def get_name(node: XMLNode, default_name: str = "default") -> str:
-    return node.get("name", default_name)
+def mj2unity_pos(pos):
+    return [-pos[1], pos[2], pos[0]]
 
-def get_pos(node: XMLNode) -> list[float]:
-    return list(map(float, node.get("pos", "0.0 0.0 0.0").split()))
 
-def get_rot(node: XMLNode) -> list[float]:
-    return list(map(float, node.get("rot", "0.0 0.0 0.0").split()))
+def mj2unity_quat(quat):
+    # note that the order is "[x, y, z, w]"
+    return [quat[2], -quat[3], -quat[1], quat[0]]
 
-def get_size(node: XMLNode) -> list[float]:
-    return list(map(float, node.get("size", "0.0 0.0 0.0").split()))
+
+def unity2mj_pos(pos):
+    return [-pos[1], pos[2], pos[0]]
+
+
+def unity2mj_quat(quat):
+    # note that the order is "[x, y, z, w]"
+    return [quat[2], -quat[3], -quat[1], quat[0]]
+
+def extract_array_from_xml(node: XMLNode, key: str, default: str = "0.0 0.0 0.0") -> list[float]:
+    return [float(x) for x in node.get(key, default).split()]
--- tests/model_test/test_model_loader.py
+++ tests/model_test/test_model_loader.py
@@ -1,31 +1,42 @@
-import mujoco
 import os
 
 from sim_pub.model_loader import MJCFLoader
-from sim_pub.mujoco.mujoco_streamer import MujocoStreamer
 
 
-def load_model(model_path: str) -> MJCFLoader:
+def test_load_model_parse(model_path: str) -> MJCFLoader:
     return MJCFLoader(model_path)
 
-def test_connect(model_path: str):
-    streamer = MujocoStreamer(model_path=model_path)
-    streamer.start_server_thread(block=True)
+def test_loader_result(loader: MJCFLoader):
+    asset_library = loader.asset_lib
+    for asset in asset_library._assets.values():
+        assert os.path.exists(asset.file_path), f"Asset file {asset.file_path} does not exist."
 
-def test_mesh_loader(model_path: str):
-    loader = load_model(model_path)
-    loader.asset_lib.include_mesh("test_mesh", "test_mesh_path")
-    print(loader.asset_lib.asset_path)
+    # for obj in loader.game_object_dict.values():
+    #     print(obj.to_dict())
+    return
+
+# def test_connect(model_path: str):
+#     streamer = MujocoStreamer(model_path=model_path)
+#     streamer.start_server_thread(block=True)
+
+    
+
+
 
 # model_path = os.path.join(os.path.dirname(__file__), "../../models/static_scene/static_scene.xml")
 # model_path = os.path.join(os.path.dirname(__file__), "../../models/pendulum/pendulum.xml")
-model_path = os.path.join(os.path.expanduser("~"), "github_code/mujoco_menagerie/franka_emika_panda/scene.xml")
+model_path = os.path.join(os.path.expanduser("~"), "github_repository/mujoco_menagerie/franka_emika_panda/scene.xml")
 model_path = os.path.abspath(model_path)
 
 if __name__ == "__main__":
 
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("model_path", help="Path to the model file")
+    # args = parser.parse_args()
+    # model_path: str = args.model_path
     # loader = load_model(model_path)
-    test_connect(model_path)
+    loader = test_load_model_parse(model_path)
+    test_loader_result(loader)
 
 
 
