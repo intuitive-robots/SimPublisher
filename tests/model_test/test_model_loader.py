c24c405d17d3f74ab5f9ece009ad81f583ee91f1
Xinkai Jiang <jiangxkjohn@gmail.com>
2024-03-28 17:38:11 +0100

    asset test

--- tests/model_test/test_model_loader.py
+++ tests/model_test/test_model_loader.py
@@ -11,30 +11,16 @@ def test_loader_result(loader: MJCFLoader):
     for asset in asset_library._assets.values():
         assert os.path.exists(asset.file_path), f"Asset file {asset.file_path} does not exist."
 
-    # for obj in loader.game_object_dict.values():
-    #     print(obj.to_dict())
+    for obj in loader.game_object_dict.values():
+        print(obj.name, len(obj.visual))
     return
 
-# def test_connect(model_path: str):
-#     streamer = MujocoStreamer(model_path=model_path)
-#     streamer.start_server_thread(block=True)
-
-    
-
-
-
-# model_path = os.path.join(os.path.dirname(__file__), "../../models/static_scene/static_scene.xml")
-# model_path = os.path.join(os.path.dirname(__file__), "../../models/pendulum/pendulum.xml")
-model_path = os.path.join(os.path.expanduser("~"), "github_repository/mujoco_menagerie/franka_emika_panda/scene.xml")
-model_path = os.path.abspath(model_path)
 
 if __name__ == "__main__":
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("model_path", help="Path to the model file")
-    # args = parser.parse_args()
-    # model_path: str = args.model_path
-    # loader = load_model(model_path)
+    home_path = os.path.expanduser("~")
+    model_path = os.path.join(home_path, "github_repository/mujoco_menagerie/franka_emika_panda/scene.xml")
+    model_path = os.path.abspath(model_path)
     loader = test_load_model_parse(model_path)
     test_loader_result(loader)
 
