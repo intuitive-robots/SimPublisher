Switched naming schema and fixed Mesh rotation

simpub/simdata.py
@@ -4,14 +4,14 @@ from typing import Optional
 
 import numpy as np
 
-class UJointType(str, Enum):
+class SimJointType(str, Enum):
   FIXED = "FIXED"
   FREE = "FREE"
   SLIDE = "SLIDE"
   BALL = "BALL"
   HINGE = "HINGE"
 
-class UVisualType(str, Enum):
+class SimVisualType(str, Enum):
   MESH = "MESH",
   BOX = "BOX",
   CYLINDER = "CYLINDER",
@@ -20,28 +20,28 @@ class UVisualType(str, Enum):
   SPHERE = "SPHERE",
   NONE = "NONE"
 
-class UAssetType(str, Enum):
+class SimAssetType(str, Enum):
   MESH = "MESH",
   MATERIAL = "MATERIAL",
   TEXTURE = "TEXTURE"
 
 @dataclass
-class UAsset:
+class SimAsset:
   tag : str
-  type : UAssetType = field(init=False)
+  type : SimAssetType = field(init=False)
 
 @dataclass
-class UMesh(UAsset):
+class SimMesh(SimAsset):
   _data : bytes
   indicesLayout : tuple[int, int] # (offset : bytes, count : int)
   normalsLayout : tuple[int, int] # (offset : bytes, count : int)
   verticesLayout : tuple[int, int] # (offset : bytes, count : int)
   uvLayout : tuple[int, int]
   hash : str = ""
-  type = UAssetType.MESH
+  type = SimAssetType.MESH
 
 @dataclass
-class UMaterial(UAsset): # https://mujoco.readthedocs.io/en/latest/XMLreference.html#asset-material
+class SimMaterial(SimAsset): # https://mujoco.readthedocs.io/en/latest/XMLreference.html#asset-material
   color : np.ndarray[np.float32]  # All the color values are within the 0 - 1.0 range 
   emissionColor : np.ndarray[np.float32] 
   specular : float = 0.5
@@ -49,14 +49,14 @@ class UMaterial(UAsset): # https://mujoco.readthedocs.io/en/latest/XMLreference.
   reflectance : float = 0
   texture : Optional[str] = None
   texsize : tuple [int, int] = (1, 1)
-  type = UAssetType.MATERIAL
+  type = SimAssetType.MATERIAL
 
 @dataclass
-class UTexture(UAsset): # https://mujoco.readthedocs.io/en/latest/XMLreference.html#asset-texture
+class SimTexture(SimAsset): # https://mujoco.readthedocs.io/en/latest/XMLreference.html#asset-texture
   width : int = 0
   height : int = 0
   textype : str = "cube"
-  type = UAssetType.TEXTURE
+  type = SimAssetType.TEXTURE
   hash : str = ""
   _data : bytes | None = None
   
@@ -65,41 +65,41 @@ class UTexture(UAsset): # https://mujoco.readthedocs.io/en/latest/XMLreference.h
 Scene data
 """
 @dataclass
-class UTransform:
+class SimTransform:
   position: np.ndarray = field(default_factory=lambda: np.zeros((3, ), dtype=np.float32))
   rotation : np.ndarray = field(default_factory=lambda: np.zeros((3, ), dtype=np.float32))
   scale : np.ndarray = field(default_factory=lambda: np.ones((3, ), dtype=np.float32))
 
 
 @dataclass
-class UVisual:
+class SimVisual:
   name : str
   type : str
   asset : str
   material : str
-  transform : UTransform
+  transform : SimTransform
   color : list[float]
 
 
 @dataclass 
-class UJoint:  
+class SimJoint:  
   name : str
-  transform : UTransform
-  body : "UBody"
+  transform : SimTransform
+  body : "SimBody"
 
-  type : UJointType = UJointType.FIXED
+  type : SimJointType = SimJointType.FIXED
   maxrot : float = 0.0
   minrot : float = 0.0
   axis : list[float] = field(default_factory=lambda: np.array([0, 0, 0]))
 
 
 @dataclass
-class UBody:
+class SimBody:
   name : str
-  visuals : list[UVisual]
-  joints : list[UJoint]
+  visuals : list[SimVisual]
+  joints : list[SimJoint]
 
-  def get_joints(self, include : set[UJointType] = {type for type in UJointType}, exclude : set[UJointType] = {UJointType.FIXED}) -> list[UJoint]:
+  def get_joints(self, include : set[SimJointType] = {type for type in SimJointType}, exclude : set[SimJointType] = {SimJointType.FIXED}) -> list[SimJoint]:
     joints = [joint for joint in self.joints if joint.type not in exclude and joint.type in include]
     found = [joint.body for joint in self.joints]
     while found and (current := found.pop()):
@@ -107,10 +107,3 @@ class UBody:
       joints += [joint for joint in connected_joints if joint.type not in exclude and joint.type in include]
       found += [joint.body for joint in connected_joints]
     return joints
-
-
-@dataclass
-class UScene:
-  id : str
-  assets : dict[str, UAsset]
-  worldbody : UBody