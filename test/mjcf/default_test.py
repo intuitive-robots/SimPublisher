287af591b935335924412aa3827c29547092f3a9
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-06-14 20:27:59 +0200

    mjcf_parser

--- test/mjcf/default_test.py
+++ test/mjcf/default_test.py
@@ -4,7 +4,7 @@ import unittest
 import numpy as np
 
 from simpub.loaders.mjcf_parser import MJCFScene
-from simpub.simdata import SimJointType, SimVisualType
+from simpub.simdata import UnityJointType, UnityVisualType
 
 
 class TestMyFunction(unittest.TestCase):
@@ -33,19 +33,18 @@ class TestMyFunction(unittest.TestCase):
 
         box = scene.worldbody.visuals[0]
         np.testing.assert_array_equal(box.color, np.array([1, 0, 0, 1]))
-        
+
         ellipsoid = scene.worldbody.bodies[0].visuals[0]
-        self.assertEqual(ellipsoid.type, SimVisualType.CAPSULE)
+        self.assertEqual(ellipsoid.type, UnityVisualType.CAPSULE)
         np.testing.assert_array_equal(ellipsoid.color, np.array([0, 1, 0, 1]))
 
-        
         sphere = scene.worldbody.bodies[0].visuals[1]
-        self.assertEqual(sphere.type, SimVisualType.SPHERE)
+        self.assertEqual(sphere.type, UnityVisualType.SPHERE)
         np.testing.assert_array_equal(sphere.color, np.array([0, 0, 1, 1]))
 
 
         cylinder = scene.worldbody.bodies[0].visuals[2]
-        self.assertEqual(cylinder.type, SimVisualType.CYLINDER)
+        self.assertEqual(cylinder.type, UnityVisualType.CYLINDER)
         np.testing.assert_array_equal(cylinder.color, np.array([1, 0, 0, 1]))
 
     def test_compiler_settings(self):
@@ -86,17 +85,17 @@ class TestMyFunction(unittest.TestCase):
         self.assertEqual(scene.worldbody.bodies[1].name, "second/third")
 
         
-        self.assertEqual(scene.worldbody.bodies[1].joints[0].type, SimJointType.FREE)
+        self.assertEqual(scene.worldbody.bodies[1].joints[0].type, UnityJointType.FREE)
 
-        self.assertEqual(scene.worldbody.visuals[0].type, SimVisualType.BOX)
-
-        
-        self.assertEqual(scene.worldbody.bodies[0].visuals[0].type, SimVisualType.SPHERE)
-        self.assertEqual(scene.worldbody.bodies[0].visuals[1].type, SimVisualType.BOX)
-        self.assertEqual(scene.worldbody.bodies[0].visuals[2].type, SimVisualType.BOX)
+        self.assertEqual(scene.worldbody.visuals[0].type, UnityVisualType.BOX)
 
         
-        self.assertEqual(scene.worldbody.bodies[0].joints[0].type, SimJointType.HINGE)
+        self.assertEqual(scene.worldbody.bodies[0].visuals[0].type, UnityVisualType.SPHERE)
+        self.assertEqual(scene.worldbody.bodies[0].visuals[1].type, UnityVisualType.BOX)
+        self.assertEqual(scene.worldbody.bodies[0].visuals[2].type, UnityVisualType.BOX)
+
+        
+        self.assertEqual(scene.worldbody.bodies[0].joints[0].type, UnityJointType.HINGE)
         self.assertListEqual(scene.worldbody.bodies[0].joints[0].axis.tolist(), [0, 0, -1])
 
 
