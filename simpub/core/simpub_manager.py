d7c583317f13ff2b4fb2a4d120b73fbba11184e6
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-08-02 10:57:33 +0200

    subscriber version 1

--- simpub/core/simpub_manager.py
+++ simpub/core/simpub_manager.py
@@ -1,6 +1,6 @@
 import abc
 import enum
-from typing import List, Dict, NewType
+from typing import List, Dict, NewType, Callable
 import threading
 from concurrent.futures import ThreadPoolExecutor, Future
 import zmq
@@ -11,6 +11,7 @@ from time import sleep
 import json
 
 IPAddress = NewType("IPAddress", str)
+Topic = NewType("Topic", str)
 
 
 class PortSet(int, enum.Enum):
@@ -20,11 +21,12 @@ class PortSet(int, enum.Enum):
 
 
 class ConnectionAbstract(abc.ABC):
-    
+
     def __init__(self):
         self.running: bool = False
         self.manager: SimPubManager = SimPubManager()
         self.host: str = self.manager.host
+        self.setting_up_socket()
         SimPubManager().add_connection(self.execute)
 
     @abc.abstractmethod
@@ -46,26 +48,33 @@ class ConnectionAbstract(abc.ABC):
 
 class SimPubManager:
 
-    _instance = None
+    _manager = None
 
     def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(SimPubManager, cls).__new__(cls, *args, **kwargs)
-        return cls._instance
+        if not cls._manager:
+            cls._manager = super(SimPubManager, cls).__new__(cls, *args, **kwargs)
+        return cls._manager
 
     def __init__(self, host: str = "127.0.0.1"):
-        self.host: str = host
+        self.host: IPAddress = host
         self.zmq_context = zmq.Context()
+        # subscriber
+        self.sub_socket_dict: Dict[IPAddress, zmq.Socket] = {}
         self.sub_socket = self.zmq_context.socket(zmq.SUB)
         self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        self.topic_map: Dict[Topic, IPAddress] = {}
+        self.host_topic: Dict[IPAddress, List[Topic]] = {}
+        self.topic_callback: Dict[Topic, Callable] = {}
+        # publisher
         self.pub_socket = self.zmq_context.socket(zmq.PUB)
         self.pub_socket.bind(f"tcp://{host}:{PortSet.TOOPIC}")
+        # service
         self.service_socket = self.zmq_context.socket(zmq.REP)
         self.service_socket.bind(f"tcp://{host}:{PortSet.SERVICE}")
-        self.topic_map: Dict[str, IPAddress] = {}
         self.service_map: Dict[str, IPAddress] = []
+        # message for broadcasting
         self.connection_map = {
-            "TOPIC": self.topic_map, 
+            "TOPIC": self.topic_map,
             "SERVICE": self.service_map
         }
         self.running: bool = False
@@ -75,8 +84,42 @@ class SimPubManager:
         self.thread = threading.Thread(target=self.thread_task)
         self.thread.start()
 
+    def join(self):
+        self.thread.join()
 
-    def broadcast(self):
+    def thread_task(self):
+        print("Server Tasks has been started")
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            self.executor = executor
+            executor.submit(self.service_loop)
+            executor.submit(self.broadcast_loop)
+            for connection in self.connections:
+                self.futures.append(executor.submit(connection.execute))
+        self.running = False
+
+    def add_connection(self, connection: ConnectionAbstract):
+        self.connections.append(connection)
+        self.executor.submit(connection.execute)
+
+    def subscribe_loop(self, host: IPAddress):
+        _socket = self.sub_socket_dict[host]
+        topic_list = self.host_topic[host]
+        while self.running:
+            message = _socket.recv_string()
+            topic, msg = message.split(":", 1)
+            if topic in topic_list:
+                self.topic_callback[topic](msg)
+
+    def service_loop(self):
+        while self.running:
+            message = self.service_socket.recv_string()
+            service, *args = message.split(":", 1)
+            if service in self.service_map:
+                self.executor.submit(self.service_map[message], *args)
+            else:
+                self.service_socket.send_string("INVALID")
+
+    def broadcast_loop(self):
         print("The server is broadcasting...")
         # set up udp socket
         _socket = socket.socket(AF_INET, SOCK_DGRAM)
@@ -85,35 +128,26 @@ class SimPubManager:
         ip_bin = struct.unpack('!I', socket.inet_aton(self.host))[0]
         netmask_bin = struct.unpack('!I', socket.inet_aton("255.255.255.0"))[0]
         broadcast_bin = ip_bin | ~netmask_bin & 0xFFFFFFFF
-        self.broadcast_ip = socket.inet_ntoa(struct.pack('!I', broadcast_bin))
+        broadcast_ip = socket.inet_ntoa(struct.pack('!I', broadcast_bin))
         while self.running:
             msg = f"SimPub:{json.dumps(self.connection_map)}"
-            _socket.sendto(msg.encode(), (self.broadcast_ip, PortSet.DISCOVERY))
+            _socket.sendto(msg.encode(), (broadcast_ip, PortSet.DISCOVERY))
             sleep(0.5)
 
-    def generate_upd_msg(self) -> bytes:
-        return 
-
-    def join(self):
-        self.thread.join()
-
-    def thread_task(self):
-        print("Server Tasks has been started")
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            self.executor = executor
-            executor.submit(self.broadcast)
-            for connection in self.connections:
-                self.futures.append(executor.submit(connection.execute))
-        self.running = False
+    def register_topic(self, topic: Topic, host: IPAddress):
+        if host in self.host_topic:
+            pass
+        self.topic_map[topic] = host
+        self.host_topic[host].append(topic)
 
     def shutdown(self):
         print("Trying to shutdown server")
         for connection in self.connections:
             connection.shutdown()
+        self.pub_socket.close(0)
+        self.service_socket.close(0)
+        for sub_socket in self.sub_socket_dict.values():
+            sub_socket.close(0)
+        self.running = False
         self.thread.join()
         print("All the threads have been stopped")
-
-    def add_connection(self, connection: ConnectionAbstract):
-        self.connections.append(connection)
-        self.executor.submit(connection.execute)
-        
--- simpub/core/subscriber.py
+++ simpub/core/subscriber.py
@@ -1,24 +1,47 @@
 import zmq
 from typing import Dict, Callable
+import json
 
-from .simpub_manager import SimPubManager, ConnectionAbstract, PortSet
+from .simpub_manager import ConnectionAbstract, PortSet
+
 
 class Subscriber(ConnectionAbstract):
-    
+
     def __init__(self, topic: str):
         super().__init__()
         self.topic: str = topic
-        
-        self._socket: zmq.Socket = self.manager.zmq_context.socket(zmq.SUB)
-        self._callback_func_list: Dict[str, Callable[[str], None]] = {}
-        self._socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        self._callback: Callable[[str], None] = {}
 
     def setting_up_socket(self):
-        self._socket.connect(f"tcp://{self.host}:{PortSet.TOOPIC}")
+        return super().setting_up_socket()
+
+    def wait_for_connection(self):
+        while self.running:
+            if self.topic not in self.manager.topic_map:
+                continue
+            host = self.manager.topic_map[self.topic]
+            if host in self.manager.sub_socket_dict:
+                self._socket = self.manager.sub_socket_dict[host]
+                break
+            self.sub_socket = self.manager.zmq_context.socket(zmq.SUB)
+            self.manager.sub_socket_dict[host] = self.sub_socket
+            self._socket.connect(f"tcp://{host}:{PortSet.TOOPIC}")
+            self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+    def register_callback(
+        self,
+        _callback: Callable[[str], None]
+    ) -> None:
+        # TODO: Make it like rospy
+        def msg_callback(msg: str):
+            json.loads(msg)
+            _callback(msg)
+
 
     def execute(self):
-        print(f"Start subscribing topic: {self.topic}")
+        print(f"Waiting for topic: {self.topic}")
         self.running = True
+        self.wait_for_connection()
         while self.running:
             message = self._socket.recv_string()
             topic, msg = message.split(":", 1)
