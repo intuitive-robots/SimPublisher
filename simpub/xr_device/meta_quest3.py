fe6aebc77a25ff2b136e83980d9acc3dfe115b1d
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-08-05 21:20:07 +0200

    vibration

--- simpub/xr_device/meta_quest3.py
+++ simpub/xr_device/meta_quest3.py
@@ -1,40 +1,48 @@
-import json
 from typing import TypedDict
 
-from simpub.server import SimPublisher
+from ..core.subscriber import Subscriber
+from ..core.publisher import Publisher
 from .xr_device import XRDevice
 
 
+class MetaQuest3Hand(TypedDict):
+    pos: list[float]
+    rot: list[float]
+    index_trigger: float
+    hand_trigger: float
+
+
 class MetaQuest3InputData(TypedDict):
-    left__pos: list[float]
-    left_rot: list[float]
-    left_index_trigger: float
-    left_hand_trigger: float
-    right_pos: list[float]
-    right_rot: list[float]
-    right_index_trigger: float
-    right_hand_trigger: float
+    left: MetaQuest3Hand
+    right: MetaQuest3Hand
     A: bool
     B: bool
     X: bool
     Y: bool
 
 
+class Viborate(TypedDict):
+    hand: str
+    intensity: float
+
+
 class MetaQuest3(XRDevice):
     def __init__(
         self,
-        publisher: SimPublisher,
-        device: str,
-        port: str = "7723",
+        device_name: str,
     ) -> None:
-        super().__init__(publisher, addr, port)
+        super().__init__(device_name)
         self.input_data: MetaQuest3InputData = None
-
-    def setup_subscribe_topic(self):
-        self.subscribe_topic("MetaQuest3/InputData", self.update)
+        self.input_subscriber = Subscriber(
+            f"{device_name}/InputData", self.update
+        )
+        self.viborate_publisher = Publisher(f"{device_name}/Vibration")
 
     def update(self, data: MetaQuest3InputData):
         self.input_data = data
 
     def get_input_data(self) -> MetaQuest3InputData:
         return self.input_data
+
+    def publish_viborate(self, signal: Viborate):
+        self.viborate_publisher.publish(signal)
