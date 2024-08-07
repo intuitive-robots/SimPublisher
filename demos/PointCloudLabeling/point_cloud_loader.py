4c4b6c8b8de7f6e492eb7f1642ac96e6ac409efd
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-08-07 21:45:29 +0200

    remove subscriber and put them all in xr_device

--- demos/PointCloudLabeling/point_cloud_loader.py
+++ demos/PointCloudLabeling/point_cloud_loader.py
@@ -2,33 +2,26 @@ import random
 import cv2
 
 import json
-from simpub.core.simpub_server import ServerBase
-import zmq
+from simpub.core.net_manager import init_net_manager
+from simpub.xr_device.xr_device import XRDevice
+
+def generate_point_cloud_data(rgb_image, depth_image):
+    positions = []
+    colors = []
+    height, width, _ = rgb_image.shape
+    for y in range(height):
+        for x in range(width):
+            rgb_pixel = rgb_image[y, x]
+            depth_pixel = depth_image[y, x]
+            if random.random() > 0.1:
+                continue
+            positions.extend([x / width, y / height, depth_pixel[0] / 10])
+            colors.extend([int(rgb_pixel[0]) / 255, int(rgb_pixel[1]) / 255, int(rgb_pixel[2]) / 255, 1])
+    return json.dumps({"positions": positions, "colors": colors})
 
 
-class RGBDPublisher(ServerBase):
-
-    def __init__(self, remote_ip: str, host: str = "127.0.0.1") -> None:
-        super().__init__(host)
-        self.request_socket = self.net_manager.zmq_context.socket(zmq.REQ)
-        self.request_socket.connect(f"tcp://{remote_ip}:7723")
-
-    def generate_point_cloud_data(self, rgb_image, depth_image):
-        positions = []
-        colors = []
-        height, width, _ = rgb_image.shape
-        for y in range(height):
-            for x in range(width):
-                rgb_pixel = rgb_image[y, x]
-                depth_pixel = depth_image[y, x]
-                if random.random() > 0.1:
-                    continue
-                positions.extend([x / width, y / height, depth_pixel[0] / 10])
-                colors.extend([int(rgb_pixel[0]) / 255, int(rgb_pixel[1]) / 255, int(rgb_pixel[2]) / 255, 1])
-        return {"positions": positions, "colors": colors}
-
-    def send_point_cloud(self, point_data) -> None:
-        self.request_socket.send_string(json.dumps(self.point_cloud_data))
+def send_point_cloud(self, point_data) -> None:
+    self.request_socket.send_string(json.dumps(self.point_cloud_data))
 
 
 rgb_path = '/home/xinkai/project/point-cloud-labeling/rgb.png'
@@ -37,6 +30,14 @@ depth_path = '/home/xinkai/project/point-cloud-labeling/depth.png'
 rgb_image = cv2.imread(rgb_path)
 depth_image = cv2.imread(depth_path)
 
-simpublisher = RGBDPublisher(rgb_image, depth_image)
-data = simpublisher.generate_point_cloud_data(rgb_image, depth_image)
-simpublisher.join()
+# simpublisher = RGBDPublisher(rgb_image, depth_image)
+net_manager = init_net_manager("127.0.0.1")
+unity_editor = XRDevice()
+# print(generate_point_cloud_data(rgb_image, depth_image))
+while unity_editor.connected is False:
+    pass
+unity_editor.request("LoadPointCloud", generate_point_cloud_data(rgb_image, depth_image))
+net_manager.join()
+
+# data = simpublisher.generate_point_cloud_data(rgb_image, depth_image)
+# simpublisher.join()
--- simpub/xr_device/xr_device.py
+++ simpub/xr_device/xr_device.py
@@ -1,6 +1,10 @@
 import json
+import zmq
+from typing import Dict, Callable
+import time
 
-from ..core.subscriber import Subscriber, logger
+from ..core.subscriber import logger
+from ..core.net_manager import NetManager, Topic, ClientPort
 
 
 class InputData:
@@ -16,8 +20,50 @@ class XRDevice:
         self,
         device_name: str = "UnityEditor",
     ) -> None:
+        self.connected = False
+        self.manager: NetManager = NetManager.manager
         self.device = device_name
-        self.log_subscriber = Subscriber(f"{device_name}/Log", self.print_log)
+        self.info = None
+        # subscriber
+        self.sub_socket = self.manager.zmq_context.socket(zmq.SUB)
+        self.sub_topic_callback: Dict[Topic, Callable] = {}
+        self.register_topic_callback(f"{device_name}/Log", self.print_log)
+        # request client
+        self.req_socket = self.manager.zmq_context.socket(zmq.REQ)
+        self.manager.submit_task(self.wait_for_connection)
+
+    def wait_for_connection(self):
+        logger.info(f"Waiting for connection to {self.device}")
+        while not self.connected:
+            client_info = self.manager.clients_info.get(self.device)
+            if client_info is not None:
+                print(client_info)
+                self.connected = True
+                self.info = client_info
+                logger.info(f"Connected to {self.device}")
+                break
+            time.sleep(0.05)
+        self.req_socket.connect(f"tcp://{self.info['ip']}:{ClientPort.SERVICE}")
+        self.sub_socket.connect(f"tcp://{self.info['ip']}:{ClientPort.TOPIC}")
+        self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        self.manager.submit_task(self.subscribe_loop)
+
+    def register_topic_callback(self, topic: str, callback: Callable):
+        self.sub_topic_callback[topic] = callback
+        self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+
+    def request(self, service: str, req: str) -> str:
+        if self.info is None or service not in self.info["services"]:
+            logger.error(f"Service {service} is not available")
+            return ""
+        self.req_socket.send_string(f"{service}:{req}")
+        return self.req_socket.recv_string()
+
+    def subscribe_loop(self):
+        while self.connected:
+            message = self.sub_socket.recv_string()
+            topic, msg = message.split(":", 1)
+            self.sub_topic_callback[topic](msg)
 
     def print_log(self, log: str):
         logger.info(f"{self.type} Log: {log}")
