2a6e5ec13da98b09dc202ed26f4893442a40d1f3
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-11-05 19:46:32 +0100

    aiohttp for asset downloading

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='simpub',
     version='0.1',
-    install_requires=["zmq", "trimesh", "pillow", "numpy", "scipy", "colorama"],
+    install_requires=["zmq", "aiohttp", "pillow", "numpy", "scipy", "colorama"],
     include_package_data=True,
     packages=['simpub', 'simpub.parser', 'simpub.sim']
 )
