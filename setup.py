4086d3e7a33276117f226e77784d05d867bca273
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-11-19 22:22:56 +0100

    new version 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simpub',
-    version='1.1',
+    version='1.2.0',
     install_requires=["zmq", "pillow", "numpy", "scipy", "colorama"],
     include_package_data=True,
     packages=['simpub', 'simpub.parser', 'simpub.sim']
