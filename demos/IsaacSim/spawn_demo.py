41cb4915fb81609aded547cf0b6ad2612890e18b
sunflower <ssfxtr@qq.com>
2024-12-03 14:35:58 +0100

    -

--- demos/IsaacSim/spawn_demo.py
+++ demos/IsaacSim/spawn_demo.py
@@ -6,10 +6,8 @@
 import argparse
 import numpy as np
 import torch
-import carb
-
 from omni.isaac.lab.app import AppLauncher
-import gymnasium as gym
+
 
 # add argparse arguments
 parser = argparse.ArgumentParser(
@@ -25,6 +23,9 @@ app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
 
 # isaac sim modules are only available after launching it (?)
+
+import carb
+import gymnasium as gym
 
 from pxr import Usd
 
@@ -360,7 +361,7 @@ def main():
 
     # start publisher
     # make sure you have a correct host address
-    publisher = IsaacSimPublisher(host="192.168.0.134", stage=sim.stage)
+    publisher = IsaacSimPublisher(host="127.0.0.1", stage=sim.stage)
     # publisher = IsaacSimPublisher(host="127.0.0.1", stage=sim.stage)
 
     # Run the simulator
