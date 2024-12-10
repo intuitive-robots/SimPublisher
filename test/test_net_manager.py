f07c827bfc0846555bb15cf1ff581f3dd2a81900
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-12-11 00:20:07 +0100

    try to fix multi broadcast problem

--- test/test_net_manager.py
+++ test/test_net_manager.py
@@ -1,7 +1,6 @@
 from simpub.core import init_net_manager
+from simpub.core.net_component import Publisher
 
-manager = init_net_manager(host="192.168.0.134")
-
-manager.start()
+manager = init_net_manager(host="127.0.0.1")
+pub_1 = Publisher("test_topic_1")
 manager.spin()
-
