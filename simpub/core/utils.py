04688ba72d293fc361f58c93fd7b684f2b7d890e
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-12-12 16:35:15 +0100

    master node auto reconnect

--- simpub/core/utils.py
+++ simpub/core/utils.py
@@ -9,6 +9,8 @@ import enum
 from .log import logger
 
 IPAddress = str
+Port = int
+Address = Tuple[IPAddress, Port]
 TopicName = str
 ServiceName = str
 AsyncSocket = zmq.asyncio.Socket
@@ -16,12 +18,15 @@ HashIdentifier = str
 
 BROADCAST_INTERVAL = 0.5
 HEARTBEAT_INTERVAL = 0.2
-DISCOVERY_PORT = 7720
+DISCOVERY_PORT = int(7720)
+
+
+class EchoHeader(enum.Enum):
+    PING = b'\x00'
+    HEARTBEAT = b'\x01'
 
 
 class MSG(enum.Enum):
-    PING = b'\x01'
-    PING_ACK = b'\x02'
     SERVICE_ERROR = b'\x03'
     SERVICE_TIMEOUT = b'\x04'
 
@@ -31,8 +36,8 @@ class NodeInfo(TypedDict):
     nodeID: str  # hash code since bytes is not JSON serializable
     ip: str
     type: str
-    servicePort: str
-    topicPort: str
+    servicePort: int
+    topicPort: int
     serviceList: List[ServiceName]
     topicList: List[TopicName]
 
@@ -67,9 +72,9 @@ def create_udp_socket() -> socket.socket:
     return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-def get_socket_port(socket: zmq.asyncio.Socket) -> str:
+def get_zmq_socket_port(socket: zmq.asyncio.Socket) -> int:
     endpoint: bytes = socket.getsockopt(zmq.LAST_ENDPOINT)  # type: ignore
-    return endpoint.decode().split(":")[-1]
+    return int(endpoint.decode().split(":")[-1])
 
 
 def split_byte(bytes_msg: bytes) -> List[bytes]:
@@ -80,14 +85,25 @@ def split_byte_to_str(bytes_msg: bytes) -> List[str]:
     return [item.decode() for item in split_byte(bytes_msg)]
 
 
+def split_str(str_msg: str) -> List[str]:
+    return str_msg.split(":", 1)
+
+
 def generate_node_msg(node_info: NodeInfo) -> bytes:
     return b"".join(
-        [node_info["nodeID"].encode(), b":", dumps(node_info).encode()]
+        [
+            EchoHeader.HEARTBEAT.value,
+            node_info["nodeID"].encode(),
+            b":",
+            dumps(node_info).encode()
+        ]
     )
 
 
 def search_for_master_node(
-    local_ip: Optional[IPAddress] = None
+    local_ip: Optional[IPAddress] = None,
+    search_time: int = 5,
+    time_out: float = 0.1
 ) -> Optional[Tuple[IPAddress, str]]:
     if local_ip is not None:
         broadcast_ip = calculate_broadcast_addr(local_ip)
@@ -96,11 +112,12 @@ def search_for_master_node(
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as _socket:
         # wait for response
         _socket.bind(("0.0.0.0", 0))
-        for _ in range(5):
-            # _, local_port = _socket.getsockname()
-            _socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            _socket.sendto(MSG.PING.value, (broadcast_ip, DISCOVERY_PORT))
-            _socket.settimeout(0.1)
+        _socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        for _ in range(search_time):
+            _socket.sendto(
+                EchoHeader.PING.value, (broadcast_ip, DISCOVERY_PORT)
+            )
+            _socket.settimeout(time_out)
             try:
                 data, addr = _socket.recvfrom(1024)
                 logger.info(f"Find a master node at {addr[0]}:{addr[1]}")
