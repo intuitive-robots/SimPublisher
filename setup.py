fc1119e0319c6c5f94ace9e0e3f93cc4bb112b70
Xinkai Jiang <jiangxinkai98@gmail.com>
2024-12-29 21:35:00 +0100

    update robocasa and metaworld example

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='simpub',
     version='1.3.1',
-    install_requires=["zmq", "pillow", "numpy", "scipy", "colorama", "opencv-python"],
+    install_requires=["zmq", "numpy", "scipy", "colorama", "opencv-python", "trimesh"],
     include_package_data=True,
     packages=['simpub', 'simpub.parser', 'simpub.sim']
 )
