8fbabf24ee8672e5087616aab2d6a52e7d710d21
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-01-26 14:53:51 +0100

    generate point cloud

--- test/test_net_manager.py
+++ test/test_net_manager.py
@@ -5,5 +5,5 @@ from simpub.xr_device.meta_quest3 import MetaQuest3
 
 manager = init_node("192.168.0.134", f"test_node_{random.randint(0, 1000)}")
 manager.start_node_broadcast()
-meta = MetaQuest3("IRLMQ3-1")
+# meta = MetaQuest3("IRLMQ3-1")
 manager.spin()
