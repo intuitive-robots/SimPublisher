12cf9ae2baa274af77f21852e36af0fe83135d8b
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-01-26 14:53:51 +0100

    generate point cloud

---
+++ test/test_pointcloud.py
@@ -0,0 +1,39 @@
+import zmq
+import time
+import numpy as np
+
+class PointCloudPublisher:
+
+    def __init__(self, ip_addr: str, topic_name: str = "PointCloud"):
+        self.topic_name = topic_name
+        self.pub_socket = zmq.Context().socket(zmq.PUB)
+        self.pub_socket.bind(f"tcp://{ip_addr}:7721")
+        self.pub_socket.setsockopt(zmq.SNDHWM, 1)
+
+    def publish_bytes(self, data: bytes) -> None:
+        msg = b''.join([f"{self.topic_name}".encode(), b"|", data])
+        self.pub_socket.send(msg)
+
+
+def generate_point() -> bytes:
+    return np.random.rand(6).astype(np.float32).tobytes()
+
+
+def generate_point_cloud(point_num: int = 10000) -> bytes:
+    point_list = []
+    for _ in range(point_num):
+        point_list.append(generate_point())
+    return b"".join(point_list)
+
+
+if __name__ == "__main__":
+
+
+    publisher = PointCloudPublisher("192.168.0.134")
+    try:
+        while True:
+            point_cloud = generate_point_cloud()
+            publisher.publish_bytes(point_cloud)
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        publisher.pub_socket.close()
