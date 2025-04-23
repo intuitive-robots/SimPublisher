ccfec5b51af0bcb4239f03d04d0cad9fdf71642a
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-04-23 17:16:37 +0200

    update point cloud loader

--- demos/PointCloudLabeling/point_cloud_loader.py
+++ demos/PointCloudLabeling/point_cloud_loader.py
@@ -2,7 +2,7 @@ import random
 import cv2
 
 import json
-from simpub.core.net_manager import init_net_manager
+from simpub.core.net_manager import init_node
 from simpub.xr_device.xr_device import XRDevice
 
 
@@ -20,11 +20,6 @@ def generate_point_cloud_data(rgb_image, depth_image):
             colors.extend([int(rgb_pixel[0]) / 255, int(rgb_pixel[1]) / 255, int(rgb_pixel[2]) / 255, 1])
     return json.dumps({"positions": positions, "colors": colors})
 
-
-def send_point_cloud(self, point_data) -> None:
-    self.request_socket.send_string(json.dumps(self.point_cloud_data))
-
-
 rgb_path = '/home/xinkai/project/point-cloud-labeling/rgb.png'
 depth_path = '/home/xinkai/project/point-cloud-labeling/depth.png'
 
@@ -32,13 +27,13 @@ rgb_image = cv2.imread(rgb_path)
 depth_image = cv2.imread(depth_path)
 
 # simpublisher = RGBDPublisher(rgb_image, depth_image)
-net_manager = init_net_manager("127.0.0.1")
+net_manager = init_node("127.0.0.1", "PointCloudPublisher")
 unity_editor = XRDevice()
 # print(generate_point_cloud_data(rgb_image, depth_image))
 while unity_editor.connected is False:
     pass
 unity_editor.request("LoadPointCloud", generate_point_cloud_data(rgb_image, depth_image))
-net_manager.join()
+net_manager.spin()
 
 # data = simpublisher.generate_point_cloud_data(rgb_image, depth_image)
 # simpublisher.join()
