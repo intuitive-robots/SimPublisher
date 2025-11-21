5a499560a1907deed53c08121f3b0ed0b5c19318
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-11-21 11:38:09 +0100

    upload to pypi

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup
 setup(
     name="simpub",
     version="2.2.0",
+    description="A cross-environment tool to publish objects from simulation for Augmented Reality and Human Robot Interaction.",
+    author="Xinkai Jiang",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    url="https://intuitive-robots.github.io/iris-project-page/",
+    license="Apache-2.0",
     install_requires=[
-        "zmq",
+        "pyzmq",
         "numpy",
         "colorama",
         "trimesh",
@@ -12,6 +17,6 @@ setup(
         "flask",
         "pyyaml",
     ],
-    include_package_data=True,
+    include_package_data=False,
     packages=["simpub", "simpub.parser", "simpub.sim"],
 )
